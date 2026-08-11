f0bcd8c693786a7503cf83a337ec69ddbdfb9c55
Mateusz <siiikor.sikorski@gmail.com>
2026-08-11 23:13:39 +0200

    Add missing marking of handled command for idempotency

--- saga/consumer.go
+++ saga/consumer.go
@@ -20,43 +20,62 @@ type ActionDispatcher[ID, TX, CTX any] interface {
 }
 
 type CommandAlreadyHandledChecker[ID any] interface {
-	check(id ID) bool
+	check(id ID) (bool, error)
 }
 
 type TransactionCompensatedChecker[TX any] interface {
-	check(tx TX) bool
+	check(tx TX) (bool, error)
+}
+
+type CommandHandledMarker[ID any] interface {
+	mark(id ID) error
 }
 
 type Consumer[ID, TX, CTX any] struct {
 	sagaActionDispatcher          ActionDispatcher[ID, TX, CTX]
 	commandAlreadyHandledChecker  CommandAlreadyHandledChecker[ID]
 	transactionCompensatedChecker TransactionCompensatedChecker[TX]
+	commandHandledMarker          CommandHandledMarker[ID]
 }
 
 func NewConsumer[ID, TX, CTX any](
 	sagaActionDispatcher ActionDispatcher[ID, TX, CTX],
 	commandAlreadyHandledChecker CommandAlreadyHandledChecker[ID],
 	transactionCompensatedChecker TransactionCompensatedChecker[TX],
+	commandHandledMarker CommandHandledMarker[ID],
 ) Consumer[ID, TX, CTX] {
 	return Consumer[ID, TX, CTX]{
 		sagaActionDispatcher:          sagaActionDispatcher,
 		commandAlreadyHandledChecker:  commandAlreadyHandledChecker,
 		transactionCompensatedChecker: transactionCompensatedChecker,
+		commandHandledMarker:          commandHandledMarker,
 	}
 }
 
-func (c Consumer[ID, TX, CTX]) Consume(ctx context.Context, command Command[ID, TX, CTX]) error {
+func (c Consumer[ID, TX, CTX]) Consume(ctx context.Context, command Command[ID, TX, CTX]) (err error) {
+	defer func() {
+		if err == nil {
+			err = c.commandHandledMarker.mark(command.Id())
+		}
+	}()
+
 	sagaAction, sagaActionFound := c.sagaActionDispatcher.SagaAction(command)
 	if !sagaActionFound {
 		return fmt.Errorf("no processor registered for command '%v'", command)
 	}
 
-	if c.commandAlreadyHandledChecker.check(command.Id()) {
+	var alreadyHandled bool
+	if alreadyHandled, err = c.commandAlreadyHandledChecker.check(command.Id()); err != nil {
+		return fmt.Errorf("checking if command '%v' handled: %w", command, err)
+	} else if alreadyHandled {
 		return nil
 	}
 
 	if tx, ok := command.ToTransaction(); ok {
-		if c.transactionCompensatedChecker.check(tx) {
+		var alreadyCompensated bool
+		if alreadyCompensated, err = c.transactionCompensatedChecker.check(tx); err != nil {
+			return fmt.Errorf("checking if tx '%v' compensated: %w", tx, err)
+		} else if alreadyCompensated {
 			return nil
 		}
 		return sagaAction.Execute(ctx, tx)
