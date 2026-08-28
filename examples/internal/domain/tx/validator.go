1b2494aac3ffa870af4cfa3c149607a753bbd865
Mateusz <siiikor.sikorski@gmail.com>
2026-08-28 15:22:01 +0200

    Implment domain validator

---
+++ examples/internal/domain/tx/validator.go
@@ -0,0 +1,31 @@
+package tx
+
+import (
+	"context"
+	"fmt"
+)
+
+type ValidatorPortOut interface {
+	Validate(ctx context.Context, registerCommand RegisterCommand) (bool, error)
+	Compensate(ctx context.Context, registerCommand RegisterCommand) error
+}
+
+type Validator struct {
+	portOut ValidatorPortOut
+}
+
+func (v Validator) validateAndCompensate(ctx context.Context, registerCommand RegisterCommand) error {
+	valid, err := v.portOut.Validate(ctx, registerCommand)
+	if err != nil {
+		return fmt.Errorf("validating %v: %w", registerCommand, err)
+	}
+
+	if valid {
+		return nil
+	}
+
+	if err = v.portOut.Compensate(ctx, registerCommand); err != nil {
+		return fmt.Errorf("compensating %v: %w", registerCommand, err)
+	}
+	return nil
+}
