d6fa2a23aa69afd9bd88fedc84cb7125dbd0d065
Mateusz <siiikor.sikorski@gmail.com>
2026-09-02 20:37:57 +0200

    Implmenet basic consumer test

--- examples/test/consumer_test.go
+++ examples/test/consumer_test.go
@@ -4,23 +4,98 @@ import (
 	"context"
 	"encoding/binary"
 	"errors"
+	"fmt"
+	"slices"
+	"sync"
+	"testing"
 
 	"github.com/mat-sik/saga-go/examples/internal/kgoconsumer"
 	"github.com/twmb/franz-go/pkg/kgo"
 )
 
 // TODO: test case when before cancel context is created, rebalance triggers and processing should be cancelled
+func TestBasicConsume(t *testing.T) {
+	topic := newTopicName(t, "basic-consume")
+	createTopic(t, topic, 1)
 
-type transientFailureRecordConsumer struct {
-	failuresRemaining int
-	failedIDs         []int
-	processedIDs      []int
+	topicDLQ := newTopicName(t, "basic-consume-dlq")
+	createTopic(t, topicDLQ, 1)
+
+	ids := []int{1, 2, 3, 4, 5}
+	prod := newProducer(t)
+	for _, id := range ids {
+		produceCommand(t, prod, topic, id)
+	}
+
+	var wg sync.WaitGroup
+
+	stubConsumer := newTransientFailureRecordConsumer(&wg, 0, len(ids))
+	client := newKgoConsumerClient(t, "basic-consume", []string{topic})
+
+	cons, err := kgoconsumer.NewConsumer(client, topicDLQ, stubConsumer.consumeRecord)
+	if err != nil {
+		t.Fatalf("creating new kgoconsumer consumer: %v", err)
+	}
+
+	consumerErrCh := make(chan error)
+	consumerCtx, cancelConsuming := context.WithCancel(t.Context())
+
+	go func() {
+		defer func() {
+			cons.Close()
+		}()
+
+		if err := cons.StartPolling(consumerCtx); err != nil {
+			consumerErrCh <- fmt.Errorf("polling: %w", err)
+			return
+		}
+		consumerErrCh <- nil
+	}()
+
+	wg.Wait()
+	cancelConsuming()
+	if err := <-consumerErrCh; err != nil && !errors.Is(err, context.Canceled) {
+		t.Fatalf("consumer polling: %v", err)
+	}
+
+	if !slices.Equal(stubConsumer.processedIDs, ids) {
+		t.Fatalf("got: %v want: %v", stubConsumer.processedIDs, ids)
+	}
 }
 
-func (c transientFailureRecordConsumer) consumeRecord(_ context.Context, record *kgo.Record) error {
+func newKgoConsumerClient(tb testing.TB, consumerGroup string, topics []string) kgoconsumer.Client {
+	consumerGroup = newConsumerGroupName(tb, consumerGroup)
+
+	client, err := kgoconsumer.NewClient(testKafkaBrokers, consumerGroup, topics)
+	if err != nil {
+		tb.Fatalf("creating new kgoconsumer client: %v", err)
+	}
+	return client
+}
+
+type transientFailureRecordConsumer struct {
+	wg           *sync.WaitGroup
+	failTimes    int
+	failedIDs    []int
+	processedIDs []int
+}
+
+func newTransientFailureRecordConsumer(wg *sync.WaitGroup, failTimes, toProcess int) transientFailureRecordConsumer {
+	wg.Add(failTimes + toProcess)
+	return transientFailureRecordConsumer{
+		wg:        wg,
+		failTimes: failTimes,
+	}
+}
+
+func (c *transientFailureRecordConsumer) consumeRecord(_ context.Context, record *kgo.Record) error {
+	defer func() {
+		c.wg.Done()
+	}()
+
 	cmd := newCommand(record)
 
-	if c.failuresRemaining > 0 {
+	if c.failTimes > len(c.failedIDs) {
 		c.failedIDs = append(c.failedIDs, cmd.id)
 		return errors.Join(errors.New("synthetic failure"), kgoconsumer.ErrTransient)
 	}
@@ -29,11 +104,11 @@ func (c transientFailureRecordConsumer) consumeRecord(_ context.Context, record 
 	return nil
 }
 
-func produceCommand(ctx context.Context, producer producer, topic string, id int) error {
+func produceCommand(tb testing.TB, producer producer, topic string, id int) {
 	cmd := command{
 		id: id,
 	}
-	return producer.produceSync(ctx, cmd.toRecord(topic))
+	producer.produce(tb, cmd.toRecord(topic))
 }
 
 type command struct {
